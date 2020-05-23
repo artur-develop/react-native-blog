5622bccfb7bdbae3e27e446c73d011e882a7ff27
ayurkevich <artur.yurkevich.it@gmail.com>
2020-05-22 23:53:18 -0400

    Component Post

---
+++ src/data.js
@@ -0,0 +1,16 @@
+export const DATA = [
+  {
+    id: '1',
+    img: 'https://i1.wp.com/paikea.ru/wp-content/uploads/2018/05/savannah-usa-04.jpg',
+    text: 'Awesome text for post 1',
+    date: new  Date().toJSON(),
+    booked: true
+  },
+  {
+    id: '2',
+    img: 'https://i1.wp.com/paikea.ru/wp-content/uploads/2018/05/savannah-usa-08.jpg',
+    text: 'Awesome text for post 2',
+    date: new  Date().toJSON(),
+    booked: true
+  }
+]
