54fdf16249f6102b265aea27efe9cd2d09d7e924
ayurkevich <artur.yurkevich.it@gmail.com>
2020-05-25 13:22:44 -0400

    Icon is added to header

--- src/screens/MainScreen.js
+++ src/screens/MainScreen.js
@@ -1,7 +1,9 @@
 import React from 'react'
 import {View,Text, StyleSheet, FlatList} from 'react-native'
+import {HeaderButtons, Item} from 'react-navigation-header-buttons'
 import {Post} from '../components/Post'
 import {DATA} from '../data'
+import {AppHeaderIcon} from '../components/AppHeaderIcon'
 
 export const MainScreen = ({navigation}) => {
   const openPostHandler = post => {
@@ -19,6 +21,19 @@ export const MainScreen = ({navigation}) => {
         renderItem={({item}) => <Post post={item} onOpen={openPostHandler}/>}
       />
     </View>
+  )
+}
+
+MainScreen.navigationOptions = {
+  headerTitle: 'My Blog',
+  headerRight: (
+    <HeaderButtons HeaderButtonComponent={AppHeaderIcon}>
+      <Item
+        title='Take photo'
+        iconName='ios-camera'
+        onPress={() => console.log('Press photo')}
+      />
+    </HeaderButtons>
   )
 }
 
