6c5b94cb0693c1832fdcf0f62d5261d814726877
ayurkevich <artur.yurkevich.it@gmail.com>
2020-05-25 14:03:30 -0400

    Bottom Tab Navigator

--- src/navigation/AppNavigation.js
+++ src/navigation/AppNavigation.js
@@ -1,7 +1,11 @@
+import React from 'react'
 import {createAppContainer} from 'react-navigation'
 import {createStackNavigator} from 'react-navigation-stack'
+import {createBottomTabNavigator} from 'react-navigation-tabs'
+import {Ionicons} from '@expo/vector-icons'
 import {MainScreen} from '../screens/MainScreen'
 import {PostScreen} from '../screens/PostScreen'
+import {BookedScreen} from '../screens/BookedScreen'
 import {THEME} from '../theme'
 
 const PostNavigator = createStackNavigator({
@@ -30,4 +34,49 @@ const PostNavigator = createStackNavigator({
   }
 })
 
-export const AppNavigation = createAppContainer(PostNavigator)
+const BookedNavigator = createStackNavigator({
+  Booked: BookedScreen,
+  Post: {
+    screen: PostScreen
+  },
+},
+  {
+    initialRouteName: 'Booked',
+    defaultNavigationOptions: {
+      headerStyle: {
+        backgroundColor: THEME.MAIN_COLOR
+      },
+      headerTintColor: '#fff'
+    }
+  })
+
+const BottomNavigator = createBottomTabNavigator({
+  Post: {
+    screen: PostNavigator,
+    navigationOptions: {
+      tabBarIcon: info => (
+        <Ionicons
+          name='ios-albums'
+          size={25}
+          color={info.tintColor}
+        />)
+    }
+  },
+  Booked: {
+    screen: BookedNavigator,
+    navigationOptions: {
+      tabBarIcon: info => (
+        <Ionicons
+          name='ios-star'
+          size={25}
+          color={info.tintColor}
+        />)
+    }
+  }
+}, {
+  tabBarOptions: {
+    activeTintColor: THEME.MAIN_COLOR
+  }
+})
+
+export const AppNavigation = createAppContainer(BottomNavigator)
