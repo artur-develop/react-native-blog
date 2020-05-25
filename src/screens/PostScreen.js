bead6d03ec7ef7b3f4bb245bf668fa73f12e8efa
ayurkevich <artur.yurkevich.it@gmail.com>
2020-05-25 12:42:02 -0400

    Post page, styles for post page

--- src/screens/PostScreen.js
+++ src/screens/PostScreen.js
@@ -1,18 +1,63 @@
 import React from 'react'
-import {View,Text, StyleSheet} from 'react-native'
+import {View,Text, StyleSheet, Image, Button, ScrollView, Alert} from 'react-native'
+import {DATA} from '../data'
+import {THEME} from '../theme'
 
-export const PostScreen = ({}) => {
+export const PostScreen = ({navigation}) => {
+  const postId = navigation.getParam('postId')
+  const post = DATA.find(p => p.id === postId)
+
+  const removeHandler = () => {
+    Alert.alert(
+      'Remove',
+      'Do you want remove a post?',
+      [
+        {
+          text: 'Cancel',
+          onPress: () => console.log('Cancel Pressed'),
+          style: 'cancel'
+        },
+        {
+          text: 'Remove',
+          onPress: () => console.log('OK Pressed'),
+          style: 'destructive'
+        }
+      ],
+      { cancelable: false }
+    )
+  }
+
   return (
-    <View style={styles.center}>
-      <Text>PostScreen</Text>
-    </View>
+    <ScrollView>
+      <Image source={{uri: post.img}} style={styles.image}/>
+      <View style={styles.textWrap}>
+        <Text style={styles.title}>{post.text}</Text>
+      </View>
+      <Button
+        title='Delete'
+        color={THEME.DANGER_COLOR}
+        onPress={removeHandler}
+      />
+    </ScrollView>
   )
 }
 
+PostScreen.navigationOptions = ({navigation}) => {
+  const postId = navigation.getParam('postId')
+  return {
+    title: `Пост ${postId}`
+  }
+}
+
 const styles = StyleSheet.create({
-  center: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center'
+  image: {
+    width: '100%',
+    height: 200
+  },
+  textWrap: {
+    padding: 10
+  },
+  title: {
+    fontFamily: 'open-regular'
   }
 })
