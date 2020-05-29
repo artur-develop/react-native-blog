cc952515158f582e77148c4ff492e16070870332
ayurkevich <artur.yurkevich.it@gmail.com>
2020-05-28 23:18:51 -0400

    Add post to db, get posts from db

--- src/store/actions/post.js
+++ src/store/actions/post.js
@@ -1,9 +1,14 @@
+import * as FileSystem from 'expo-file-system'
 import {LOAD_POSTS, REMOVE_POST, TOGGLE_BOOKED, ADD_POST} from '../types'
+import {DB} from '../../db'
 
 export const loadPosts = () => {
-  return {
-    type: LOAD_POSTS,
-    payload: []
+  return async dispatch => {
+    const posts = await DB.getPosts()
+    dispatch({
+      type: LOAD_POSTS,
+      payload: posts
+    })
   }
 }
 
@@ -21,10 +26,24 @@ export const removePost = id => {
   }
 }
 
-export const addPost = post => {
-  post.id = Date.now().toString()
-  return {
-    type: ADD_POST,
-    payload: post
+export const addPost = post => async dispatch => {
+  const fileName = post.img.split('/').pop()
+  const newPath = FileSystem.documentDirectory + fileName
+
+  try {
+    await FileSystem.moveAsync({
+      to: newPath,
+      from: post.img
+    })
+  } catch(e) {
+    console.log('Error: ', e)
   }
+  const payload = {...post, img: newPath}
+  const id = await DB.createPost(payload)
+  payload.id = id
+
+  dispatch({
+    type: ADD_POST,
+    payload
+  })
 }
